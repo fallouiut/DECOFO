e03f8bfd4decb0a1f9f90fb7cd62214972b16a07
fallou <=>
2021-03-23 02:36:12 +0100

    allow-origin on filterGIT

--- src/main/java/m2info/ter/decofo/auth/filter/AuthFilter.java
+++ src/main/java/m2info/ter/decofo/auth/filter/AuthFilter.java
@@ -33,7 +33,7 @@ public class AuthFilter implements Filter {
             if(token == null) throw new ServletException("Non Authentifié");
             if(authManager.isTokenExpired(token)) throw new ServletException("Token expiré");
         }
-        
+
         ((HttpServletResponse)servletResponse).addHeader("Access-Control-Allow-Headers", "*");
         ((HttpServletResponse)servletResponse).addHeader("Access-Control-Allow-Origin", "*");
         ((HttpServletResponse)servletResponse).addHeader("Access-Control-Allow-Methods", "GET,OPTIONS,HEAD,PUT,POST");
